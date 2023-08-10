7e2f03f8109510f5558e35fcd34f6459c85eea84
santoshsatnak <santoshsatnak11@gmail.com>
2023-08-10 15:34:05 +0530

    JSON Assignment 1st
    JSON Assignment 1st

---
+++ 14_jsonAssign1.js
@@ -0,0 +1,37 @@
+let jsonEmployeeInfo = `{
+    "name": "Aleix Melon",
+    "id": "E00245",
+    "role": ["Dev", "DBA"],
+    "doj": "11-12-2019",
+    "married" : false,
+    "address": {
+     "stress": "32, Laham St.",
+     "city": "Innsbruck",
+     "country": "Austria"
+    },
+    "referred-by" : "E0012"
+
+ }`;
+ const employee = JSON.parse(jsonEmployeeInfo); // Convert from JSON format to object
+ const arrayRole = employee.role;
+ for (const role of arrayRole) {
+     if (role=="Dev") {
+         console.log(role);
+     }
+ }
+
+   const employeeName = employee.name;
+     let employeeSplit = employeeName.split(" ");
+     console.log(employeeSplit);
+
+
+     let employeeLength = employeeSplit[employeeSplit.length-1];
+     console.log(employeeLength);
+
+     const employeeDoj = employee.doj;
+     let employeeSplit1 = employeeDoj.split("-");
+     console.log(employeeSplit1);
+
+     let employeeYear = employeeSplit1[employeeSplit1.length-1];
+     console.log(employeeYear);
+
