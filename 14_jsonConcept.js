74630774abc9a1a67d05c8969dd56d807dc357e0
santoshsatnak <santoshsatnak11@gmail.com>
2023-08-10 15:22:53 +0530

    JSON Assignments
    JSON Assignments

---
+++ 14_jsonConcept.js
@@ -0,0 +1,85 @@
+let addressStr = `{
+
+    "flatNo": 307,
+
+    "building": "B",
+
+    "street": "Datta Mandir Road",
+
+    "location": "Wakad",
+
+    "pin": 411057,
+
+    "owners": ["Lalu Devant", "Shalu Devant" ]
+
+}`;
+
+
+
+ 
+
+const address = JSON.parse(addressStr); // Convert from JSON format to object
+
+console.log(`type of addressStr  ==> ${typeof addressStr}`);
+
+console.log(` address type of ==>  ${typeof address}`);
+
+console.log(address);
+
+console.log(address.location);
+
+console.log(address.owners);
+
+ 
+
+console.log(`==========================`);
+
+const student = {
+
+    rollNo: 123,
+
+    fullName: "Jenny Atand",
+
+    marks: "98%",
+
+    awards: {
+
+        hsc: "School topper",
+
+        ssc: "Division Topper",
+
+        graduation: "Gold Medalist"
+
+    }
+
+}
+
+let studentStr = JSON.stringify(student); // convert object to json 
+
+console.log(studentStr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
